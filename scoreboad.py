0242d08ea0fc41b0ac1e3edcb41eaea88be5346c
Hiba Altaf <54900941+hibathecreator@users.noreply.github.com>
2021-08-25 16:01:07 -0400

    Update scoreboad.py

--- scoreboad.py
+++ scoreboad.py
@@ -1,6 +1,6 @@
 
 from turtle import Turtle
-
+import os
 ALIGNMENT = 'center'
 FONT = ('Courier', 24, 'normal')
 
@@ -8,6 +8,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as file:
+            if os.path.getsize('data.txt') > 0:
+                self.high_score = int(file.read())
+            else:
+                self.high_score = 0
+                file.write(str(self.high_score))
         self.color('white')
         self.penup()
         self.goto(0,265)
@@ -15,14 +21,18 @@ class Scoreboard(Turtle):
         self.update_scoreboad()
 
     def update_scoreboad(self):
-        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f'Score: {self.score} High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.home()
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', 'w') as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboad()
 
     def increment_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboad()
 
